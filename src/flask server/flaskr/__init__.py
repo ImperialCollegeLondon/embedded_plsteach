55b7554be9379570780bddbeaa6cb7d116cdebb0
samw212 <40913680+samwan62@users.noreply.github.com>
2019-02-08 15:55:42 +0000

    flaskr added

---
+++ src/flask server/flaskr/__init__.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  1 10:04:54 2019
+
+@author: Sam Wan
+"""
+import os
+
+from flask import Flask, render_template, g, redirect, url_for
+from flask_mqtt import Mqtt
+from flask_socketio import SocketIO
+
+socketio = SocketIO()
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True) #instance folder is relative
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'), # .../instance/flaskr.sqlite saved only,
+                                                                    #init. done in db.py
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True) #search done in instance folder
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route('/')
+    def index():
+        if g.user is None:
+            return render_template('index.html')
+        else:
+            return redirect(url_for('main.home'))
+            
+    
+    from . import db
+    db.init_app(app) #register command and set up teardown
+    
+    from . import auth
+    app.register_blueprint(auth.bp)
+    
+    from. import main
+    app.register_blueprint(main.bp)
+    
+    #mqtt = Mqtt(app)
+    socketio.init_app(app)
+    return app
---
+++ src/flask server/flaskr/auth.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  1 11:29:21 2019
+Views: handle and respond incoming  requests
+Blueprints: group related views
+Blueprints registered to application when available
+This file contains the authentication views (blueprint)
+@author: Sam Wan
+"""
+
+import functools
+
+from flask import (
+        Blueprint, flash, g, redirect, render_template, request, session, url_for
+        )
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from flaskr.db import get_db
+
+bp = Blueprint('auth', __name__, url_prefix='/auth') #url prefix 
+
+@bp.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form['username'] #dict like
+        password = request.form['password']
+        db = get_db()
+        error = None
+        
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif db.execute(
+                'SELECT id FROM user WHERE username =?', (username,)).fetchone() is not None:
+            error = 'User {} is alreqady registered.'.format(username)
+        
+        if error is None:
+            db.execute(
+                    'INSERT INTO user (username, password) VALUES (?,?)',
+                    (username, generate_password_hash(password)))
+            db.commit() #require to save data modifications
+            return redirect(url_for('auth.login')) #generate redirect response 
+                                                    #url based on view name
+                                                    #link is prepended to bp
+        
+        flash(error)
+        
+    return render_template('auth/register.html') #return html
+
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            session.clear() #session is dict that stores data across requests
+            session['user_id'] = user['id'] #user info loaded and made available to other views after logins
+            return redirect(url_for('index'))
+
+        flash(error)
+
+    return render_template('auth/login.html')
+
+@bp.before_app_request # function that runs before any view
+def load_logged_in_user(): #checks if a user id is in a session, then fetch data
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
+        
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+        
+        return view(**kwargs)
+    
+    return wrapped_view
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+        
