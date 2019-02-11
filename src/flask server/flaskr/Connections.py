0f25940a1d747fc1606d2447d7c9ac70c164201e
t-tht <thtangthomas@gmail.com>
2019-02-11 18:20:36 +0000

    get value from pi

--- src/flask server/flaskr/Connections.py
+++ src/flask server/flaskr/Connections.py
@@ -14,37 +14,49 @@ from flaskr.auth import login_required
 from flaskr.db import get_db
 from flask_socketio import emit, Namespace
 from . import socketio
+from flask_mqtt import Mqtt
+
+__value = 0
+__time = 0
 
 class Connections(Namespace):
-        
+
     def __init__(self, queue_length, namespace):
         super(Namespace, self).__init__(namespace)
         self.queue = Queue(10)
         self.evt = threading.Event()
-       
+
     def on_connect(self):
         #mqtt
+        sub = 'IC.embedded/plzteach/thomas'
+        topic = 'IC.embedded/plzteach/test'
+        broker = 'test.mosquitto.org'
+        app = Flask(__name__)
+        app.config['MQTT_BROKER_URL'] = broker
+        app.config['MQTT_BROKER_PORT'] = 1883
+        mqtt = Mqtt(app)
+
         print("WS Client is CONNECTED")
         self.sender = Consumer(self.queue, self.evt, True)
-        self.grabber = Producer(self.queue, self.evt, True)
+        self.grabber = Producer(self.queue, self.evt, True, mqtt)   #need to pass mqtt object
         self.grabber.start()
         self.sender.start()
         print("Threads are STARTED")
         socketio.emit('Ready')
-    
+
     def on_start_transmit(self):
         self.evt.set()
         print('Event is SET')
-        
+
     def on_stop_transmit(self):
         self.evt.clear()
         print('Event is CLEARED')
-        
+
     def on_disconnect(self):
         self.evt.clear()
         self.grabber.runThreads = False #kill threads
         self.sender.runThreads = False #kill threads
-    
+
     def on_save(self):
         db = get_db()
         error = None
@@ -52,13 +64,13 @@ class Connections(Namespace):
         title = "testing"
         if not title:
             error = "Please name your record."
-    
+
         elif  db.execute(
                     'SELECT title FROM sess_records WHERE title =?', (title,)).fetchone() is not None:
                 error = 'Title {} is already there.'.format(title) #no replace
-        
+
         flash(error)
-        
+
         if error is None:
             js = self.sender.generateJS()
             db.execute(
@@ -67,16 +79,16 @@ class Connections(Namespace):
             db.commit()
             print("Successfully saved.")
         return self.sender.gen_JS()
-                
+
 class Consumer(threading.Thread):
-    
+
     def __init__(self, queue, event, runThreads):
         threading.Thread.__init__(self)
         self.data = queue
         self.event = event
         self.runThreads = runThreads
         self.list = []
-    
+
     def run(self):
         while self.runThreads:
             self.event.wait()
@@ -90,10 +102,10 @@ class Consumer(threading.Thread):
                 socketio.emit('Sensor_Dc')
                 self.runThreads = False
             time.sleep(0.2)
-    
+
     def gen_JS(self):
         return json.dumps(dict(list))
-            
+
 class Producer(threading.Thread):
 
     def __init__(self, queue, event, runThreads):
@@ -101,15 +113,34 @@ class Producer(threading.Thread):
         self.data = queue
         self.event = event
         self.runThreads = runThreads
-        
+        self.mqtt = mqtt
+
     def run(self):
-        while self.runThreads:
-            self.event.wait()
-            try: 
-                tmp = (1,1)
-                self.data.put(tmp,True, 5)
-                print("PUT", tmp)
-            except Queue.full:
-                print("Queue is full")
-                self.runThreads = False
-            time.sleep(0.2)
+    while self.runThreads:
+        self.event.wait()
+        self.mqtt.subscribe("IC.embedded/plzteach/thomas")
+        try:
+            @self.mqtt.on_message()
+            def handle_messages(client, userdata, message):
+                msg = (message.payload).decode('utf-8')
+                msg_dict = json.loads(msg)
+                t=msg_dict["time"]
+                v=msg_dict["result"]
+                set_value(v,t)
+            v,t = read_value()
+            self.data.put([v,t],True, 5)
+            print("PUT", v)
+        except Queue.full:
+            print("Queue is full")
+            self.runThreads = False
+        time.sleep(0.2)
+
+def read_value(): #getter
+    global __value
+    global __time
+    return __value, __time
+def set_value(y, x): #setter
+    global __value
+    global __time
+    __value = y
+    __time = x
--- src/flask server/flaskr/main.py
+++ src/flask server/flaskr/main.py
@@ -33,6 +33,8 @@ def home():
 def plot():
     #chdeck db for settings for mqtt
     #call initilization
+    mqtt.subscribe(sub) #currently doesn't do anything, need to pass to producer object
+                    #atm there's a subscribe call in producer.run()
     return render_template('main/plot.html')
 
 @bp.route('/status', methods=('GET','POST'))
@@ -49,14 +51,14 @@ def status():
         sensor_name = request.form['sensor_name']
         pin_no = request.form['pin']
         print('Posted', sensor_name, pin_no)
-        error = None        
+        error = None
         config = config_table[pin_no]
-        
+
         if len(g.user_setting) >=4:
             error = 'You can have at most 4 sensors.'
         elif sensor_name in g.user_settings:
             error = 'Sensor already exists.'
-        
+
         if error is None:
             db.execute(
                     'INSERT INTO settings (user_id, sensor_name, config) VALUES (?,?,?)',
@@ -65,7 +67,7 @@ def status():
             return redirect(url_for('main.status'))
         flash(error)
     else:
-        
+
         return render_template('main/status.html')
 
 @bp.route('/widget_settings', methods = ('GET', 'POST'))
@@ -79,53 +81,3 @@ def widget_settings():
 @login_required
 def view():
     return render_template('main/view.html')
-
-############################################################
-
-DEBUG = 0
-
-@mqtt.on_connect()
-def handle_connect(client,userdata,flags,rc):
-    print('Connected to broker: ' + broker)
-
-@mqtt.on_message()
-def handle_messages(client, userdata, message):
-    msg = (message.payload).decode('utf-8')
-    msg_dict = json.loads(msg)
-    t=msg_dict["time"]
-    res=msg_dict["result"]
-    #return res
-    print("t={}\t\t\t\tval={}".format(t,res))
-    if DEBUG:
-        passprint('Received message on topic {}: {}'
-          .format(message.topic, message.payload.decode()))
-
-@mqtt.on_publish()
-def handle_publish(client, userdata, mid):
-    if DEBUG:
-        print('Published message with mid {}.'
-          .format(mid))
-@mqtt.on_subscribe()
-def handle_subscribe(client, userdata, mid, granted_qos):
-    if DEBUG:
-        print('Subscription id {} granted with qos {}.'
-          .format(mid, granted_qos))
-
-@mqtt.on_disconnect()
-def handle_disconect():
-    print('Disconnected')
-"""
-@mqtt.on_log()
-def handle_logging(client, userdata, level, buf):
-    print(client, userdata, level, buf)
-"""
-############################################################
-
-mqtt.subscribe(sub)
-
-
-i = 0
-def gen_data():
-    global i
-    i = i+0.1
-    return i.np.random.randint(10)
