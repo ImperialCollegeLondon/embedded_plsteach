a1a28f984d42cd72107e8beafe4407432189cbf7
samw212 <40913680+samwan62@users.noreply.github.com>
2019-02-12 18:21:25 +0000

    add

--- src/datatest.py
+++ src/datatest.py
@@ -8,49 +8,62 @@ print("waiting for 3 s...")
 time.sleep(3)
 port_no = 8884
 
-
 client = mqtt.Client()
 client.tls_set(ca_certs="mosquitto.org.crt", certfile="client.crt",keyfile="client.key")
 print("before connect")
 client.connect("test.mosquitto.org", port = port_no)
 print("after connect")
-client.publish("IC.embedded/plzteach/on", "I'M GOING IN")
+client.publish("IC.embedded/plzteach/on", "ready")
 
 def on_message(client, userdata, message) :
 	global loop_flag
-#       print("Received message:{} on topic {}".format(message.payload, message$
+	loop_flag = False
+	global pause
+	if (message.payload).decode('utf-8') == "pause":
+		pause = True
+		print("paused")
+	if (message.payload).decode('utf-8') == "unpause":
+		pause = False
+	global stop
+	if (message.payload).decode('utf-8') == "stop":
+		stop = True
+		pause = True
 	global setup
 	setup = (message.payload).decode('utf-8')
 	print(setup)
-	loop_flag = 0
+	
 
 client.on_message = on_message
 client.subscribe("IC.embedded/plzteach/config")
+client.loop_start()
 
 while 1:
-	loop_flag = 1
-	client.loop_start()
-	while loop_flag == 1:
+	loop_flag = True
+	pause = False
+	stop = False
+	while loop_flag == True:
 		print("waiting")
 		time.sleep(10)
 
-	client.loop_stop()
 	ADDR = 0x48
 	CONFIG_REG_1 = ast.literal_eval(setup)
 	bus = sb.SMBus(1)
-	begin=time.perf_counter()
+	begin = time.perf_counter()
+	pause_time = 0
 
-	def get_nconversion(config, N):
-		for x in range(N):
-			for sensor in config:
+	while stop == False:
+		while pause == False:
+			for sensor in CONFIG_REG_1:
 				bus.write_i2c_block_data(ADDR, 1, sensor)
 				data = bus.read_i2c_block_data(ADDR, 0 ,2)
-				result=int.from_bytes(data, 'big')/32768*4.096
-				payload=json.dumps({'time':time.perf_counter()-begin,hex(sensor[0]):result})
+				result = int.from_bytes(data, 'big')/32768*4.096
+				global real_time
+				real_time = time.perf_counter() - begin - pause_time
+				payload=json.dumps({'time':real_time,hex(sensor[0]):result})
 				client.publish("IC.embedded/plzteach/result", payload)
 				print(payload)
+		pause_time = time.perf_counter() - begin - real_time
+	client.publish("IC.embedded/plzteach/on", "stopped")
+	print("stopped")
 
-	get_nconversion(CONFIG_REG_1, 5000)
-
-	client.publish("IC.embedded/plzteach/on", "done")
-
+client.loop_stop()
